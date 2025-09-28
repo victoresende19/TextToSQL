b346466613a2621542abf9201a13893b883b03b0
Victor Resende <victoresende19@gmail.com>
2025-09-27 23:50:02 -0300

    :star: api dialect :star:

--- api/db/engine.py
+++ api/db/engine.py
@@ -2,16 +2,10 @@
 
 from sqlalchemy import create_engine
 
-def create_db_engine(dialect: str):
+def create_db_engine(dialect: str, string_connection: str = None):
     """Cria e retorna um engine do SQLAlchemy com base no dialeto."""
-    if dialect.lower() == 'sqlserver':
-        connection_string = "mssql+pyodbc://<USUARIO>:<SENHA>@<SERVIDOR>/<BANCO>?driver=ODBC+Driver+17+for+SQL+Server"
-        return create_engine(connection_string)
-    elif dialect.lower() == 'oracle':
-        connection_string = "oracle+oracledb://<USUARIO>:<SENHA>@<HOSTNAME>:<PORTA>/<SERVICE_NAME>"
-        return create_engine(connection_string)
-    elif dialect.lower() == 'sqlite':
+    if dialect.lower() == 'sqlite':
         # Usa o arquivo de banco de dados físico
         return create_engine("sqlite:///db/database.db")
-    else:
-        raise ValueError("Dialeto não suportado.")
+
+    return create_engine(string_connection)
